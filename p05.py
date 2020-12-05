b95978a80518cd9ef34224a69575adb4465eee7e
Derek <sjukledighet@gmail.com>
2020-12-05 04:06:19 -0500

    use more efficient method for part 2
    clean up code

--- p05.py
+++ p05.py
@@ -2,11 +2,9 @@ from aocd import data as boarding_passes
 
 
 table = str.maketrans('FBLR', '0101')
-seats = []
-
-for boarding_pass in boarding_passes.splitlines():
-    seats.append(int(boarding_pass.translate(table), 2))
+seats = [int(bp.translate(table), 2) for bp in boarding_passes.splitlines()]
+M = min(seats) - 1
+L = len(seats) + 1
 
 print('Part 1:', max(seats))
-my_seat = set(range(min(seats), max(seats))) - set(seats)
-print('Part 2:', my_seat.pop())
+print('Part 2:', (L**2 + L) // 2 + L * M - sum(seats))
