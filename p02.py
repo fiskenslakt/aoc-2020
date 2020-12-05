e6ebb6c3157a6ca23b1e1c83943cfc7534c2ca55
Derek <sjukledighet@gmail.com>
2020-12-05 04:32:33 -0500

    stop using collections.Counter and make code more concise

--- p02.py
+++ p02.py
@@ -1,5 +1,4 @@
 import re
-from collections import Counter
 
 from aocd import data as policies
 
@@ -8,18 +7,10 @@ valid_sled = 0
 valid_toboggan = 0
 
 for policy in policies.splitlines():
-    policy = re.findall(r'(\w+)', policy)
-    low = int(policy[0])
-    high = int(policy[1])
-    letter = policy[2]
-    password = policy[3]
-    frequency = Counter(password)
+    lo, hi, char, pw = re.findall(r'(\w+)', policy)
 
-    if low <= frequency[letter] <= high:
-        valid_sled += 1
-
-    if (password[low-1] == letter) ^ (password[high-1] == letter):
-        valid_toboggan += 1
+    valid_sled += int(lo) <= pw.count(char) <= int(hi)
+    valid_toboggan += (pw[int(lo)-1] == char) ^ (pw[int(hi)-1] == char)
 
 print('Part 1:', valid_sled)
 print('Part 2:', valid_toboggan)
